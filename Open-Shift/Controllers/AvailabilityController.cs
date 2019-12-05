cf1ef4cf5f322b7b0b8bf819c3e5a3995eb9178a
Joe <jdboris@yahoo.com>
2019-12-05 02:50:07 -0500

    Continued working on availabilities

--- Open-Shift/Controllers/AvailabilityController.cs
+++ Open-Shift/Controllers/AvailabilityController.cs
@@ -20,16 +20,31 @@ namespace Open_Shift.Controllers
                 return Redirect("~/Login");
             }
 
-            Models.Database db = new Database();
-            AvailabilityViewModel viewModel = null;
+            AvailabilityViewModel viewModel = new AvailabilityViewModel
+            {
+                User = new User()
+                {
+                    blnIsManager = Models.User.IsManager.Manager,
+                    StoreID = Models.User.StoreLocationList.Kotetsu,
+                    AssociateID = 2
+                },
+                availabilities = null
+            };
 
             if (Request.QueryString["m"] != null && Request.QueryString["y"] != null)
             {
+                Models.Database db = new Database();
+
+                /*
                 viewModel = new AvailabilityViewModel
                 {
                     User = u,
                     availabilities = db.GetAvailabilities(u.StoreID, Convert.ToInt32(Request.QueryString["m"]), Convert.ToByte(Request.QueryString["m"]), u.AssociateID)
                 };
+                */
+                // TODO: Stop hard-coding this and use the real data from the database
+                viewModel.availabilities = db.GetAvailabilities(u.StoreID, Convert.ToInt32(Request.QueryString["m"]), Convert.ToByte(Request.QueryString["m"]), u.AssociateID);
+                
             }
 
             return View(viewModel);

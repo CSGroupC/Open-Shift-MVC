b0f8fc061ca06b9835aadcb5903176cc36224f37
Annie <43357774+LaserA@users.noreply.github.com>
2019-12-08 00:07:32 -0500

    Profile page prefilles
    *Profile prefills when user signs in
    **css needs work
    *profile/update cancel button works

--- Open-Shift/Controllers/Profile.cs
+++ Open-Shift/Controllers/Profile.cs
@@ -94,6 +94,9 @@ namespace Open_Shift.Controllers
                 }
                 Models.User u = new Models.User();
                 Models.Home h = new Models.Home();
+
+                if (col["btnSubmit"] == "cancel") return RedirectToAction("Index", "Home");
+
                 {
                     u = Models.User.GetUserSession();
 

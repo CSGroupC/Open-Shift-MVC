26de82747751b41aa28f5161e8bbf915dda0abb3
jdboris <54695266+jdboris@users.noreply.github.com>
2019-12-08 02:23:09 -0500

    Finished the batch files

--- hide-database-changes.bat
+++ hide-database-changes.bat
@@ -1,2 +1,3 @@
-﻿git update-index --assume-unchanged Open-Shift\\App_Data\\OpenShift.mdf
+﻿
+git update-index --assume-unchanged Open-Shift\\App_Data\\OpenShift.mdf
 git update-index --assume-unchanged Open-Shift\\App_Data\\OpenShift_log.ldf
--- show-database-changes.bat
+++ show-database-changes.bat
@@ -1,2 +1,3 @@
-﻿git update-index --no-assume-unchanged Open-Shift\\App_Data\\OpenShift.mdf
+﻿
+git update-index --no-assume-unchanged Open-Shift\\App_Data\\OpenShift.mdf
 git update-index --no-assume-unchanged Open-Shift\\App_Data\\OpenShift_log.ldf

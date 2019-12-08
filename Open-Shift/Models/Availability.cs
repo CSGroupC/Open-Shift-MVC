7925e1232cda32b3f569012354c94c0f65273215
jdboris <54695266+jdboris@users.noreply.github.com>
2019-12-08 15:27:28 -0500

    Worked on availability

--- Open-Shift/Models/Availability.cs
+++ Open-Shift/Models/Availability.cs
@@ -57,5 +57,16 @@ namespace Open_Shift.Models
             }
             catch (Exception ex) { throw new Exception(ex.Message); }
         }
+
+        public bool Delete()
+        {
+            try
+            {
+                Database db = new Database();
+                db.DeleteAvailability(this.ID);
+                return true;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
     }
 }
